fc6539e20dcff2a47407eb1ed2de22f2639b8c7f
Aline Borges <alinekborges@gmail.com>
2018-05-30 18:03:44 -0300

    Added more testing to retryWhenNeeded function

--- InstaNorrisTests/Utils/RetrySpec.swift
+++ InstaNorrisTests/Utils/RetrySpec.swift
@@ -34,8 +34,8 @@ class RetrySpec: QuickSpec {
                 
                 let observable = Observable.deferred { () -> Observable<Int> in
                     count += 1
-                    print("count \(count)")
                     guard responses.count >= count else {
+                        assert(false, "Observable called too many times")
                         return .empty()
                     }
                     return .error(responses[count - 1])
@@ -48,9 +48,37 @@ class RetrySpec: QuickSpec {
                         errorCount += 1
                     }).disposed(by: self.disposeBag)
                 
-                expect(count).toEventually(be(2), timeout: 20)
-                expect(errorCount).toEventually(be(1), timeout: 20)
+                expect(count).toEventually(be(2), timeout: 12.1) //Also testing correct ellapsed time
+                expect(errorCount).toEventually(be(1), timeout: 12.1)
                 
+            }
+            
+            it("Don't retry if not the right errror") {
+                
+                var errorCount = 0
+                var count = 0
+                
+                let error = MoyaError.underlying(NSError(domain: "", code: 404, userInfo: nil), nil)
+                
+                let responses = [error, error, error]
+                
+                let observable = Observable.deferred { () -> Observable<Int> in
+                    count += 1
+                    guard count <= 1 else {
+                        assert(false, "Observable called too many times")
+                        return .empty()
+                    }
+                    return .error(responses[count - 1])
+                }
+                
+                observable
+                    .retryWhenNeeded()
+                    .subscribe(onNext: { _ in
+                    }, onError: { _ in
+                        errorCount += 1
+                    }).disposed(by: self.disposeBag)
+                
+                expect(errorCount).toEventually(be(1), timeout: 20)
             }
             
         }
